5ccfd2065e6c569db784636d031070dd3be0badd
tocjack <jack.yin@tomatocart.com>
2013-09-17 14:00:57 +0800

    [V2.0 Alhpa5] Add invoice module into the admin panel
    -- Add the invoice menu to the customers menu

---
+++ admin/system/modules/access/invoices.php
@@ -0,0 +1,32 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ * TomatoCart
+ *
+ * An open source application ecommerce framework
+ *
+ * @package   TomatoCart
+ * @author    TomatoCart Dev Team
+ * @copyright Copyright (c) 2011, TomatoCart, Inc.
+ * @license   http://www.gnu.org/licenses/gpl-3.0.html
+ * @link    http://tomatocart.com
+ * @since   Version 0.5
+ * @filesource
+ */
+
+  class TOC_Access_Invoices extends TOC_Access 
+  {
+	    public function __construct()
+	    {
+		      parent::__construct();
+		      
+		      $this->_module = 'invoices';
+		      $this->_group = 'customers';
+		      $this->_icon = 'invoices.png';
+		      $this->_sort_order = 400;
+		      
+		      $this->_title = lang('access_invoices_title');
+	    }
+  }
+
+/* End of file orders.php */
+/* Location: ./system/modules/access/invoices.php */

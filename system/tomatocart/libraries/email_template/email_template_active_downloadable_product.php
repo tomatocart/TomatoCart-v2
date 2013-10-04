4096f4f2987f29ee0cff2f1fa52901a239275c8c
tocjack <jack.yin@tomatocart.com>
2013-10-04 23:26:55 +0800

    Active Downlodable Product - Update the private to protected for template name property

--- system/tomatocart/libraries/email_template/email_template_active_downloadable_product.php
+++ system/tomatocart/libraries/email_template/email_template_active_downloadable_product.php
@@ -33,7 +33,7 @@ class TOC_Email_Template_active_downloadable_product extends TOC_Email_Template_
 	/**
 	 * Email Template Name
 	 *
-	 * @access private
+	 * @access protected
 	 * @var string
 	 */
 	protected $template_name = 'active_downloadable_product';

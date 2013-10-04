69052eceeb5e4d7f574cb6bfe329dc8b6cb8d872
tocjack <jack.yin@tomatocart.com>
2013-10-04 23:28:59 +0800

    create order credit slip - Update the private to protected for template name property

--- system/tomatocart/libraries/email_template/email_template_admin_create_order_credit_slip.php
+++ system/tomatocart/libraries/email_template/email_template_admin_create_order_credit_slip.php
@@ -33,7 +33,7 @@ class TOC_Email_Template_admin_create_order_credit_slip extends TOC_Email_Templa
 	/**
 	 * Email Template Name
 	 *
-	 * @access private
+	 * @access protected
 	 * @var string
 	 */
 	protected $template_name = 'admin_create_order_credit_slip';
